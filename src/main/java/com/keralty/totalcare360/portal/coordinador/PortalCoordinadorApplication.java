2e36a49aa01724be075d8b600ef2563d05e08f43
Jesus Fernando Noy Burbano (Lider Técnico) <jfnoy@keralty.com>
2023-04-27 11:58:24 -0500

    ajustes aplicacion para utilizar Swagger

---
+++ src/main/java/com/keralty/totalcare360/portal/coordinador/PortalCoordinadorApplication.java
@@ -0,0 +1,29 @@
+package com.keralty.totalcare360.portal.coordinador;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@ApplicationPath("/")
+@OpenAPIDefinition(
+        tags = {
+                @Tag(name="widget", description="Widget operations."),
+                @Tag(name="gasket", description="Operations related to gaskets")
+        },
+        info = @Info(
+                title="Portal Coordinador API",
+                version = "1.0.1",
+                contact = @Contact(
+                        name = "API Support",
+                        url = "http://exampleurl.com/contact",
+                        email = "techsupport@example.com"),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html"))
+)
+public class PortalCoordinadorApplication extends Application{
+}
